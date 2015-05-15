bcd8782d0f8e6e8c5081ef149f2b98259096b625
superdyoll <superdyoll@gmail.com>
2015-05-15 11:29:05 +0100

    Register User

--- src/users/Register.java
+++ src/users/Register.java
@@ -8,13 +8,17 @@ package users;
 import databases.SQLite;
 import databases.Database;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import security.SHA512;
 
 /**
  *
  * @author Lloyd
  */
 public class Register {
-    
+
     Database db;
     Connection con;
 
@@ -22,78 +26,62 @@ public class Register {
         this.db = new SQLite();
         con = db.getConnection();
     }
-    
-    public Register(Database db){
+
+    public Register(Database db) {
         this.db = db;
         con = db.getConnection();
     }
-    
-     if (strlen($password) != 128) {
-        // The hashed pwd should be 128 characters long.
-        // If it's not, something really odd has happened
-        $error_msg .= '<p class="error">Invalid password configuration.</p>';
-    }
 
-    // Username validity and password validity have been checked client side.
-    // This should should be adequate as nobody gains any advantage from
-    // breaking these rules.
-    //
-    
-    $prep_stmt = "SELECT id FROM members WHERE email = ? LIMIT 1";
-    $stmt = $mysqli->prepare($prep_stmt);
-    
-    if ($stmt) {
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $stmt->store_result();
-        
-        if ($stmt->num_rows == 1) {
-            // A user with this email address already exists
-            $error_msg .= '<p class="error">A user with this email address already exists.</p>';
+    public boolean RegisterUser(String firstName, String lastName, String userName, String password) throws SQLException {
+
+        String error_msg = "";
+
+        if (password.length() != 128) {
+            // The hashed pwd should be 128 characters long.
+            // If it's not, something really odd has happened
+            error_msg += "Invalid password configuration.";
         }
-    } else {
-        $error_msg .= '<p class="error">Database error</p>';
-    }
-	$stmt->close();
-	//check username availability
-    $prep_stmt = "SELECT id FROM members WHERE username = ? LIMIT 1";
-    $stmt = $mysqli->prepare($prep_stmt);
-    
-    if ($stmt) {
-        $stmt->bind_param('s', $username);
-        $stmt->execute();
-        $stmt->store_result();
-        
-        if ($stmt->num_rows == 1) {
-            // A user with this email address already exists
-            $error_msg .= '<p class="error">A user with this username already exists.</p>';
-        }
-    } else {
-        $error_msg .= '<p class="error">Database error</p>';
-    }
-	$stmt->close();
-    // TODO: 
-    // We'll also have to account for the situation where the user doesn't have
-    // rights to do registration, by checking what type of user is attempting to
-    // perform the operation.
 
-    if (empty($error_msg)) {
-        // Create a random salt
-        $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
-
-        // Create salted password 
-        $password = hash('sha512', $password . $random_salt);
-
-        // Insert the new user into the database 
-        if ($insert_stmt = $mysqli->prepare("INSERT INTO members (username, email, password, salt, grav_email) VALUES (?, ?, ?, ?, ?)")) {
-            $insert_stmt->bind_param('sssss', $username, $email, $password, $random_salt, $email);
-            // Execute the prepared query.
-            if (! $insert_stmt->execute()) {
-                header('Location: error.php?err=Registration failure: INSERT');
-                exit();
+        // Username validity and password validity have been checked client side.
+        // This should should be adequate as nobody gains any advantage from
+        // breaking these rules.
+        //
+        String prep_stmt = "SELECT U_ID FROM User WHERE uname = ? LIMIT 1";
+        try (PreparedStatement stmt = con.prepareStatement(prep_stmt)) {
+            stmt.setString(1, userName);
+            ResultSet rs = stmt.executeQuery();
+            
+            if (rs.next()) {
+                // A user with this email address already exists
+                error_msg += "A user with this email address already exists.</p>";
             }
         }
-        header('Location: register_success.php');
-        exit();
+
+        if (error_msg.isEmpty()) {
+            SHA512 hasher = new SHA512();
+            // Create a random salt
+            String random_salt = hasher.getHashedRandomSalt();
+
+            // Create salted password 
+            password = hasher.getHashedPassword(password, random_salt);
+
+            // Insert the new user into the database 
+            PreparedStatement insert_stmt = con.prepareStatement("INSERT INTO members (username, email, password, salt, grav_email) VALUES (?, ?, ?, ?, ?)");
+
+            insert_stmt.setString(1, firstName);
+            insert_stmt.setString(2, lastName);
+            insert_stmt.setString(3, userName);
+            insert_stmt.setString(4, password);
+            insert_stmt.setString(5, random_salt);
+
+            // Execute the prepared query.
+            if (!insert_stmt.execute()) {
+                return false;
+
+            }
+            return true;
+        }
+        return false;
+
     }
 }
